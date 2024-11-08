71c752e2105eaa8362584b45db01d4903a2c059c
Marc Frankfurter <marcfrankfurter@gmail.com>
2024-11-08 22:56:11 +0100

    progress

---
+++ src/lib/home.svelte
@@ -0,0 +1,16 @@
+<section
+  id="home"
+  class="min-h-screen flex items-center justify-center nav-bar-scroll-margin relative overflow-hidden"
+>
+  <div class="absolute inset-0 w-full h-full"></div>
+  <div class="text-center relative z-10">
+    <h1
+      class="text-5xl font-bold mb-6 bg-gradient-to-r from-red-500 to-purple-500 text-transparent bg-clip-text"
+    >
+      Welcome to my Portfolio!
+    </h1>
+    <p class="text-xl text-gray-400 max-w-2xl mx-auto">
+      Here you can find information about me and my projects.
+    </p>
+  </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+  import SectionContainer from "$lib/section-container.svelte";
+  import Home from "$lib/home.svelte";
+</script>
+
+<Home />
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<SectionContainer>Hell yhea</SectionContainer>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
