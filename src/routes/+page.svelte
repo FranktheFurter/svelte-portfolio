d1209e3b1f9668f7bc70e77d8f6153035dd02c77
Marc Frankfurter <johndoe@example.commarcfrankfurter@gmail.com>
2024-11-09 12:57:59 +0100

    cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
-  import Hero from "$lib/Hero.svelte";
-  import About from "$lib/About.svelte";
-  import Projects from "$lib/Projects.svelte";
-  import Contact from "$lib/Contact.svelte";
+  import Hero from "$lib/sections/Hero.svelte";
+  import About from "$lib/sections/About.svelte";
+  import Projects from "$lib/sections/Projects.svelte";
+  import Contact from "$lib/sections/Contact.svelte";
 </script>
 
 <Hero />
